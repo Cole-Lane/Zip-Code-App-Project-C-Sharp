cdd6bb1ae60149c707f6d5a6030dc0c9ec844906
Cole Lane <dcole.lane@gmail.com>
2023-06-25 13:15:18 -0400

    Remove unused using statements.

--- Zip Code App Project/ZipModel.cs
+++ Zip Code App Project/ZipModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Zip_Code_App_Project
+﻿namespace Zip_Code_App_Project
 {
     /// <summary>
     /// Object model to store the response from the zippopotam.us API
